339ec76201c9bc33bdb710189ca95fba81f904cb
Sid <siddheshchalke45@gmail.com>
2018-04-24 14:16:35 -0400

    1) ProjectControl added(RegisterMethodOverloadControl). 2) More clean up.(Removed machine dependent files)

--- ProjectControls/Enumerations.cs
+++ ProjectControls/Enumerations.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using ProjectControls.Helper;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DependencyPropertyGenerator
+namespace ProjectControls
 {
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
     public enum ResiterMethodOverload
     {
         [Description("With No Argument")]
